53b184f9b7e0bac557432eb160519aaf8ac5845e
arrowandbow12 <zachyc426@gmail.com>
2024-07-27 11:44:05 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,28 @@
 kDriverControllerPort = 0
+
+class ShooterConstants:
+    kEncoderPorts = (4, 5)
+    kEncoderReversed = False
+    kEncoderCPR = 1024
+    kEncoderDistancePerPulse = 1 / kEncoderCPR
+
+    kShooterMotorPort = 4
+    kFeederMotorPort = 5
+
+    kShooterFreeRPS = 5300
+    kShooterTargetRPS = 4000
+    kShooterToleranceRPS = 50
+
+    # These are not real PID gains, and will have to be tuned for your specific robot.
+    kP = 1
+    kI = 0
+    kD = 0
+
+    # On a real robot the feedforward constants should be empirically determined; these are
+    # reasonable guesses.
+    kSVolts = 0.05
+
+    # Should have value 12V at free speed...
+    kVVoltSecondsPerRotation = 12.0 / kShooterFreeRPS
+
+    kFeederSpeed = 0.5
