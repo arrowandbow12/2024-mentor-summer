0555fd1597cab1e008dea03d2e8c26721b1bded7
arrowandbow12 <zachyc426@gmail.com>
2024-07-27 11:38:08 -0700

    New Shooter Subsystem

--- robotcontainer.py
+++ robotcontainer.py
@@ -8,6 +8,7 @@
 
 # project imports
 import subsystems.drive.drivetrain
+import subsystems.shooter.shootersubsystem
 import constants
 
 # wpi imports
@@ -16,6 +17,7 @@ import wpimath
 import commands2
 import commands2.cmd
 import commands2.button
+
 
 # vendor imports
 
@@ -32,6 +34,32 @@ class RobotContainer:
         """The container for the robot. Contains subsystems, OI devices, and commands."""
         # The robot's subsystems
         self.robotDrive = subsystems.drive.drivetrain.Drivetrain(isReal=isReal)
+        self.shooter = subsystems.shooter.shootersubsystem.ShooterSubsystem()
+
+        self.spinUpShooter = commands2.cmd.runOnce(self.shooter.enable, self.shooter)
+        self.stopShooter = commands2.cmd.runOnce(self.shooter.disable, self.shooter)
+
+        self.autonomousCommand = commands2.cmd.sequence(
+            # Start the command by spinning up the shooter...
+            commands2.cmd.runOnce(self.shooter.enable, self.shooter),
+            # Wait until the shooter is at speed before feeding the frisbees
+            commands2.cmd.waitUntil(lambda: self.shooter.getController().atSetpoint()),
+            # Start running the feeder
+            commands2.cmd.runOnce(self.shooter.runFeeder, self.shooter),
+            # Shoot for the specified time
+            commands2.cmd.waitSeconds(constants.AutoConstants.kAutoShootTimeSeconds)
+            # Add a timeout (will end the command if, for instance, the shooter
+            # never gets up to speed)
+            .withTimeout(constants.AutoConstants.kAutoTimeoutSeconds)
+            # When the command ends, turn off the shooter and the feeder
+            .andThen(
+                commands2.cmd.runOnce(
+                    lambda: self.shooter.disable, self.shooter
+                ).andThen(
+                    commands2.cmd.runOnce(lambda: self.shooter.stopFeeder, self.shooter)
+                )
+            ),
+        )
 
         # The driver's controller
         self.driverController = wpilib.XboxController(
@@ -64,6 +92,41 @@ class RobotContainer:
         factories on commands2.button.CommandGenericHID or one of its
         subclasses (commands2.button.CommandJoystick or command2.button.CommandXboxController).
         """
+        self.driverController.a().onTrue(self.spinUpShooter)
+
+        # Turn off the shooter when the 'B' button is pressed
+        self.driverController.b().onTrue(self.stopShooter)
+
+        # We can also write them as temporary variables outside the bindings
+
+        # Shoots if the shooter wheel has reached the target speed
+        shoot = commands2.cmd.either(
+            # Run the feeder
+            commands2.cmd.runOnce(self.shooter.runFeeder, self.shooter),
+            # Do nothing
+            commands2.cmd.none(),
+            # Determine which of the above to do based on whether the shooter has reached the
+            # desired speed
+            lambda: self.shooter.getController().atSetpoint(),
+        )
+
+        stopFeeder = commands2.cmd.runOnce(self.shooter.stopFeeder, self.shooter)
+
+        # Shoot when the 'X' button is pressed
+        self.driverController.x().onTrue(shoot).onFalse(stopFeeder)
+
+        # We can also define commands inline at the binding!
+
+        # While holding the shoulder button, drive at half speed
+        self.driverController.rightBumper().onTrue(
+            commands2.cmd.runOnce(
+                lambda: self.robotDrive.setMaxOutput(0.5), self.robotDrive
+            )
+        ).onFalse(
+            commands2.cmd.runOnce(
+                lambda: self.robotDrive.setMaxOutput(1), self.robotDrive
+            )
+        )
 
     def getAutonomousCommand(self) -> commands2.Command:
         """
@@ -71,4 +134,4 @@ class RobotContainer:
 
         :returns: the command to run in autonomous
         """
-        return commands2.InstantCommand()
+        return self.autonomousCommand #or commands2.InstantCommand()
